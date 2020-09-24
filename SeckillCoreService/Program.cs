a3fea06c0e11f81d1ac2890ffe323fba7114fc92
MR.Zhang <36786472+wei19941001@users.noreply.github.com>
2020-09-24 11:21:26 +0800

    添加项目文件。

---
+++ SeckillCoreService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SeckillCoreService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
